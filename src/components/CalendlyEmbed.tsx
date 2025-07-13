1cbfb1ef5cc85a16cfa6821743be8efb67d9e66a
drebarrera <drebarrera@yahoo.com>
2025-07-13 18:22:38 -0500

    Fixed red calendly background loading screen

--- src/components/CalendlyEmbed.tsx
+++ src/components/CalendlyEmbed.tsx
@@ -13,7 +13,7 @@ export default function CalendlyEmbed() {
   }, []);
 
   return (
-    <div className="w-[500px] h-[525px] scale-[80%] flex items-center justify-center overflow-clip bg-red-500 mt-[-10%]"  style={{ borderRadius: "20px" }}>
+    <div className="w-[500px] h-[525px] scale-[80%] flex items-center justify-center overflow-clip bg-white mt-[-10%]"  style={{ borderRadius: "20px" }}>
       <div className="w-full h-[525px]" style={{ borderRadius: "20px" }}>
         <iframe src="https://calendly.com/drebarrera/chat?embed_domain=localhost%3A3000&amp;embed_type=Inline&amp;hide_event_type_details=1&amp;hide_gdpr_banner=1" width="100%" height="100%" title="Select a Date &amp; Time - Calendly"></iframe>
       </div>
